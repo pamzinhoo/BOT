a18d585030e5552f5d413423a936a8e11281695f
Pam <joaovictormatos1414@gmail.com>
2026-09-08 14:32:17 -0300

    feat: register dashboard DLC API

--- api/routes/admin/__init__.py
+++ api/routes/admin/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from fastapi import APIRouter
 
+from api.routes.admin.dlcs_router import router as dlcs_router
 from api.routes.admin.events_router import router as events_router
 from api.routes.admin.giveaways_router import router as giveaways_router
 from api.routes.admin.router import router as legacy_router
@@ -15,6 +16,7 @@ router.include_router(settings_patch_router)
 router.include_router(settings_router)
 router.include_router(events_router)
 router.include_router(giveaways_router)
+router.include_router(dlcs_router)
 router.include_router(legacy_router)
 
 __all__ = ["router"]
