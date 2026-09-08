cd4987245a34588f566a4fc82eeb135085053f78
Pam <joaovictormatos1414@gmail.com>
2026-09-08 20:11:00 -0300

    feat: register monetization plan management router

--- api/routes/admin/__init__.py
+++ api/routes/admin/__init__.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter
 from api.routes.admin.dlcs_router import router as dlcs_router
 from api.routes.admin.events_router import router as events_router
 from api.routes.admin.giveaways_router import router as giveaways_router
+from api.routes.admin.monetization_plans_router import router as monetization_plans_router
 from api.routes.admin.monetization_router import router as monetization_router
 from api.routes.admin.panels_router import router as panels_router
 from api.routes.admin.router import router as legacy_router
@@ -22,6 +23,7 @@ router.include_router(events_router)
 router.include_router(giveaways_router)
 router.include_router(dlcs_router)
 router.include_router(panels_router)
+router.include_router(monetization_plans_router)
 router.include_router(monetization_router)
 router.include_router(staff_router)
 router.include_router(ticket_actions_router)
